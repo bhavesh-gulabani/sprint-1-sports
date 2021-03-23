8acf2753746a7e8cb84f2cda9fe68297278c3359
Chinmayi Joshi <79636352+chinickks@users.noreply.github.com>
2021-03-23 08:14:38 +0530

    Added exceptions

---
+++ src/main/java/com/cg/exception/ControllerExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.cg.exception;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.WebRequest;
+
+// Custom exceptions are meant for business rule violation
+
+import com.cg.util.ErrorDetail;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+	@ResponseBody
+	@ExceptionHandler(CustomerNotFoundException.class)
+	ResponseEntity<?> exceptionHandler(CustomerNotFoundException ex, WebRequest request) {
+		ErrorDetail errorDetail = new ErrorDetail(new Date(), ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity<>(errorDetail, HttpStatus.NOT_FOUND);
+	}
+	
+	@ResponseBody
+	@ExceptionHandler(ProductNotFoundException.class)
+	ResponseEntity<?> exceptionHandler(ProductNotFoundException ex, WebRequest request) {
+		ErrorDetail errorDetail = new ErrorDetail(new Date(), ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity<>(errorDetail, HttpStatus.NOT_FOUND);
+	}
+	
+	@ResponseBody
+	@ExceptionHandler(UserNotFoundException.class)
+	ResponseEntity<?> exceptionHandler(UserNotFoundException ex, WebRequest request) {
+		ErrorDetail errorDetail = new ErrorDetail(new Date(), ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity<>(errorDetail, HttpStatus.NOT_FOUND);
+	}
+}
