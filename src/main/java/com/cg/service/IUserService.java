a4ffc921f0546ecde80a80b4255e6c6856629665
Chinmayi Joshi <79636352+chinickks@users.noreply.github.com>
2021-03-23 08:16:14 +0530

    Added addUser

--- src/main/java/com/cg/service/IUserService.java
+++ src/main/java/com/cg/service/IUserService.java
@@ -2,12 +2,22 @@ package com.cg.service;
 
 import java.util.List;
 
-import com.cg.bean.User;
+import javax.transaction.Transactional;
 
+import org.springframework.stereotype.Service;
+
+import com.cg.bean.User;
+import com.cg.exception.UserNotFoundException;
+
+@Service
+@Transactional
 public interface IUserService {
-	public User signIn(User user);
+	public User signIn(User user) throws UserNotFoundException;
 	public User signOut(User user);
-	public User changePassword(long id, User user);
+	public User changePassword(long id, User user)  throws UserNotFoundException;
 	public List<User> getAllUsers();
-	public User getUserById(long custId);
+	public User getUserById(long id) throws UserNotFoundException;
+	public User removeUser(long id) throws UserNotFoundException;
+	public User addUser(User user);
+
 }
