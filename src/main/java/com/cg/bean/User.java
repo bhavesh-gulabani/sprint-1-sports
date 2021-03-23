e82915a772a46e7e72996f7393d12fd1283af079
Chinmayi Joshi <79636352+chinickks@users.noreply.github.com>
2021-03-23 08:09:58 +0530

    Added equals method

--- src/main/java/com/cg/bean/User.java
+++ src/main/java/com/cg/bean/User.java
@@ -7,18 +7,23 @@ import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
 @Entity
-@Table(name = "System_User")
-@Inheritance(strategy=InheritanceType.JOINED)
+@Table(name = "System_user")
+@Inheritance(strategy = InheritanceType.JOINED)
 public class User {
+
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long userId;
+	private long id;
+	@NotBlank(message = "Username cannot be blank")
 	private String username;
+	@NotBlank(message = "Password cannot be blank")
 	private String password;
 	private String role;
-	
+
 	// Constructors
 	public User() {
 		super();
@@ -32,12 +37,12 @@ public class User {
 	}
 
 	// Getters and setters
-	public long getUserId() {
-		return userId;
+	public long getId() {
+		return id;
 	}
-	
-	public void setUserId(long userId) {
-		this.userId = userId;
+
+	public void setId(long id) {
+		this.id = id;
 	}
 	
 	public String getUsername() {
@@ -63,11 +68,52 @@ public class User {
 	public void setRole(String role) {
 		this.role = role;
 	}
-		
+
 	// toString
 	@Override
 	public String toString() {
-		return "User [userId=" + userId + ", username=" + username + ", password=" + password + ", role=" + role + "]";
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ", role=" + role + "]";
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((role == null) ? 0 : role.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id != other.id)
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (role == null) {
+			if (other.role != null)
+				return false;
+		} else if (!role.equals(other.role))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}	
+	
 	
 }
